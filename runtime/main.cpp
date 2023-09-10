14f2fb0e1fae31813813629a4f4364ed46a5ed76
qimenlang <510391246@qq.com>
2023-09-10 14:45:44 +0800

    add AddGitSubmodule.cmake

--- runtime/main.cpp
+++ runtime/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <TinyLib.h>
+#include <nlohmann/json.hpp>
+
 
 int main(){
     PrintHello();
+
+    std::cout << "JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "."
+              << NLOHMANN_JSON_VERSION_MINOR << "."
+              << NLOHMANN_JSON_VERSION_PATCH << "\n";
     return 0;
 }
